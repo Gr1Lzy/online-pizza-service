9e8905bab6f13b1f2004886486a22d6fd05b7c54
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-08-12 23:46:23 +0300

    Create UserDto

---
+++ src/main/java/org/github/application/dto/user/UserDto.java
@@ -0,0 +1,9 @@
+package org.github.application.dto.user;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String fullName;
+}
---
+++ src/main/java/org/github/application/dto/user/UserSaveDto.java
@@ -0,0 +1,4 @@
+package org.github.application.dto.user;
+
+public class UserSaveDto {
+}
