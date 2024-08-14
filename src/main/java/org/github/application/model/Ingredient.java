9ebffec2acab070b26c734bef8b67c8c3aef8fb7
gr1lzy <kolomoets02@gmail.com>
2024-08-14 23:15:23 +0300

    add Ingredient

---
+++ src/main/java/org/github/application/model/Ingredient.java
@@ -0,0 +1,38 @@
+package org.github.application.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToMany(mappedBy = "ingredients")
+    @ToString.Exclude
+    private Set<Product> products;
+}
