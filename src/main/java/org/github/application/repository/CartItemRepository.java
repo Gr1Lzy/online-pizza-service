453f47e347f6d94c4b22ee24217b21f3242fb874
Gr1Lzy <kolomoets02@gmail.com>
2024-08-19 15:59:36 +0300

    add repositories

---
+++ src/main/java/org/github/application/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package org.github.application.repository;
+
+import org.github.application.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
---
+++ src/main/java/org/github/application/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package org.github.application.repository;
+
+import org.github.application.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
