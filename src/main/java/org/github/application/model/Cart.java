570205416445131bd07be75a896eb7529b3646f7
Gr1Lzy <kolomoets02@gmail.com>
2024-08-19 11:50:53 +0300

    create new model

---
+++ src/main/java/org/github/application/model/Cart.java
@@ -0,0 +1,37 @@
+package org.github.application.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = true)
+    private User user;
+
+    @Column(name = "total_amount", nullable = false, columnDefinition = "BIGINT DEFAULT 0")
+    private Long totalAmount;
+
+    @Column(name = "token", nullable = false)
+    private String token;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
