0f0826b99aa4e25d5acedf3f51fac72922bf4199
Gr1Lzy <kolomoets02@gmail.com>
2024-08-19 14:13:59 +0300

    create BaseEntity util

---
+++ src/main/java/org/github/application/util/BaseEntity.java
@@ -0,0 +1,32 @@
+package org.github.application.util;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
